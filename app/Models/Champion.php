3f136ba0b657f8c99121c5ed4d2b253a0175b649
LelekM <lelekyt@gmail.com>
2023-01-23 10:55:07 +0100

    Dodanie $actualHp $maxHp $baseHp Dodanie funkcji addHp addArmor decreaseHp decreaseArmor recivePhysicalDamage

--- app/Models/Champion.php
+++ app/Models/Champion.php
@@ -3,25 +3,29 @@ namespace App\Models;
 class Champion
 {
     public $name;
-    public $hp;
+    public $actualHp;
+    public $maxHp;
+    public $baseHp;
     public $armor;
     public $items = [];
     public function __construct($data)
     {
         $this->name = $data["name"];
-        $this->hp = $data["hp"];
+        $this->actualHp = $data["hp"];
+        $this->baseHp = $data["hp"];
         $this->armor = $data["armor"];
+        $this->maxHp = $this->baseHp;
     }
 
     public function addItem($data)
     {
-        if (count($this->items) > 6) {
+        if (count($this->items) > 5) {
             echo "MAX ITEMOW";
             return;
         }
         $this->items [] = $data;
-        $this->hp = $this->hp + $data["hp"]; //do wyjebania, dodac funkcje co podlicza ile mam aktualnie max hp, odpalana po dodaniu itemu albo odjeciu
-        $this->armor = $this->armor + $data["armor"]; // same situation
+        $this->addHp($data["hp"]); //do wyjebania, dodac funkcje co podlicza ile mam aktualnie max hp, odpalana po dodaniu itemu albo odjeciu
+        $this->addArmor($data["armor"]); // same situation
     }
 
     public function deleteItem($name)
@@ -30,6 +34,8 @@ class Champion
         {
             if ($item["name"] == $name )
             {
+                $this->decreaseArmor($item["armor"]);
+                $this->decreaseHp($item["hp"]);
                 unset($this->items[$key]);
                 return true;
             }
@@ -38,6 +44,29 @@ class Champion
     }
     public function receivePhysicalDamage($dmg)
     {
-
+        $dmgReduction = $this->armor / ($this->armor + 100);
+//        var_dump(round($dmgReduction, 2));
+        $this->actualHp = $this->actualHp - ($dmg - round(($dmg * round($dmgReduction, 2)), 0));
     }
+
+    public function addHp($data)
+    {
+        $this->actualHp = $this->actualHp + $data;
+        $this->maxHp = $this->maxHp + $data;
+    }
+    public function addArmor($data)
+    {
+        $this->armor = $this->armor + $data;
+    }
+
+    public function decreaseHp($data)
+    {
+        $this->actualHp = $this->actualHp - $data;
+        $this->maxHp = $this->maxHp - $data;
+    }
+    public function decreaseArmor($data)
+    {
+        $this->armor = $this->armor - $data;
+    }
+
 }
