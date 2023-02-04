23d70cb93893856355ef2c610044dbd8c014c6f0
Lelek <lelekyt@gmail.com>
2023-02-04 21:02:07 +0100

    Poprawienie updateStats Poprawienie testow

--- app/Models/Champion.php
+++ app/Models/Champion.php
@@ -1,9 +1,14 @@
 <?php
+
 namespace App\Models;
+
 use PDO;
 
 class Champion
 {
+    public static $experienceThreshold = [[0, 99], [100, 199], [200, 299], [300, 399], [400, 499], [500, 599],
+        [600, 699], [700, 799], [800, 899], [900, 999], [1000, 1099], [1100, 1199], [1200, 1299],
+        [1300, 1399], [1400, 1499], [1500, 1599], [1600, 1699], [1700, 100000]];
     public $name;
     public $actualHp;
     public $maxHp;
@@ -18,9 +23,6 @@ class Champion
     public $magicResistGrowth;
     public $level;
     public $experience;
-    public static $experienceTreshold = [[0,99],[100, 199],[200, 299], [300, 399],[400,499],[500,599],
-        [600,699],[700,799],[800,899],[900,999],[1000,1099],[1100, 1199], [1200, 1299],
-        [1300, 1399], [1400, 1499],[1500,1599],[1600,1699],[1700,100000]];
 
     public function __construct($data)
     {
@@ -39,22 +41,76 @@ class Champion
         $this->experience = 0;
     }
 
+    public static function getAll()
+    {
+        $query = \App\Services\Db::get()->prepare('select * from lelek.champions;');
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $champions = [];
+        foreach ($query as $row) {
+            $champions [] = $row;
+        }
+        return $champions;
+    }
+
+    public static function addNew(string $name, int $hp, int $magicResist, int $armor, int $hpGrowth, int $armorGrowth, int $magicResistGrowth)
+
+    {
+        $query = \App\Services\Db::get()->prepare("insert into lelek.champions (name, hp, magicResist, armor, hpGrowth, armorGrowth, magicResistGrowth) values ('$name', '$hp','$magicResist', '$armor', '$hpGrowth',  '$armorGrowth', '$magicResistGrowth')");
+        $query->execute();
+        return static::find($name);
+    }
+
+    public static function find($name)
+    {
+        $query = \App\Services\Db::get()->prepare("select * from lelek.champions where name ='$name';");
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $data = $query->fetchAll()[0];
+        $champion = new \App\Models\Champion($data);
+        return $champion;
+    }
+
     public function addItem(Item $item)
     {
         if (count($this->items) > 5) {
-            echo "MAX ITEMOW";
+            echo "MAX ITEMS";
             return;
         }
         $this->items [] = $item;
         $this->updateStats();
     }
 
+    public function updateStats()
+    {
+        $bonusHp = 0;
+        $bonusArmor = 0;
+        $bonusMr = 0;
+        foreach ($this->items as $item) {
+            $bonusHp += $item->hp;
+            $bonusArmor += $item->armor;
+            $bonusMr += $item->magicResist;
+        }
+
+        $scalingHp = ($this->level - 1) * $this->hpGrowth;
+        $scalingArmor = ($this->level - 1) * $this->armorGrowth;
+        $scalingMr = ($this->level - 1) * $this->magicResistGrowth;
+
+        $bonusHp += $scalingHp;
+        $bonusArmor += $scalingArmor;
+        $bonusMr += $scalingMr;
+
+        $hpDiff = $this->maxHp - $this->actualHp;
+        $this->maxHp = $this->baseHp + $bonusHp;
+        $this->actualHp = $this->maxHp - $hpDiff;
+        $this->armor = $this->baseArmor + $bonusArmor;
+        $this->magicResist = $this->baseMr + $bonusMr;
+    }
+
     public function deleteItem($name)
     {
-        foreach ($this->items as $key=>$item)
-        {
-            if ($item->name == $name )
-            {
+        foreach ($this->items as $key => $item) {
+            if ($item->name == $name) {
                 unset($this->items[$key]);
                 $this->updateStats();
                 return true;
@@ -66,18 +122,7 @@ class Champion
     public function addLevel()
     {
         $this->level = $this->level + 1;
-        $this->experience = Champion::$experienceTreshold[$this->level-1][0];
-        $this->updateStats();
-    }
-
-    public function setLevel(int $level, $experience = null)
-    {
-        $levelDiff = $level - $this->level;
-        $this->level = $this->level + $levelDiff;
-        if(is_null($experience))
-        {
-            $this->experience = Champion::$experienceTreshold[$this->level-1][0];
-        }
+        $this->experience = Champion::$experienceThreshold[$this->level - 1][0];
         $this->updateStats();
     }
 
@@ -93,82 +138,30 @@ class Champion
         $this->actualHp = $this->actualHp - ($dmg - round(($dmg * $dmgReduction), 0));
     }
 
-
-    public static function getAll()
-    {
-        $query = \App\Services\Db::get()->prepare('select * from lelek.champions;');
-        $query->execute();
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        $champions = [];
-        foreach ($query as $row){
-            $champions []= $row;
-        }
-        return $champions;
-}
-    public static function find($name)
-    {
-        $query = \App\Services\Db::get()->prepare("select * from lelek.champions where name ='$name';");
-        $query->execute();
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        $data = $query->fetchAll()[0];
-        $champion = new \App\Models\Champion($data);
-        return $champion;
-    }
-
-    public static function addNew(string $name, int $hp, int $magicResist, int $armor, int $hpGrowth, int $armorGrowth, int $magicResistGrowth)
-
-    {
-        $query = \App\Services\Db::get()->prepare("insert into lelek.champions (name, hp, magicResist, armor, hpGrowth, armorGrowth, magicResistGrowth) values ('$name', '$hp','$magicResist', '$armor', '$hpGrowth',  '$armorGrowth', '$magicResistGrowth')");
-        $query->execute();
-        return static::find($name);
-    }
-
-
-
-    public function checkLevel($experience = null)
-    {
-        for($i = 0; $i <=17; $i++)
-        {
-            if ($this->experience >= Champion::$experienceTreshold[$i][0] && $this->experience <= Champion::$experienceTreshold[$i][1])
-
-            {
-                $this->setLevel(($i+1), $experience);
-                return;
-            }
-        }
-    }
-
     public function addExperience(int $experience)
     {
         $this->experience = $this->experience + $experience;
         $this->checkLevel($experience);
     }
 
-    public function updateStats()
+    public function checkLevel($experience = null)
     {
-        $bonusHp = 0;
-        $bonusArmor = 0;
-        $bonusMr = 0;
-        foreach ($this->items as $item)
-        {
-           $bonusHp += $item->hp;
-           $bonusArmor += $item->armor;
-           $bonusMr += $item->magicResist;
+        for ($i = 0; $i <= 17; $i++) {
+            if ($this->experience >= Champion::$experienceThreshold[$i][0] && $this->experience <= Champion::$experienceThreshold[$i][1]) {
+                $this->setLevel(($i + 1), $experience);
+                return;
+            }
         }
+    }
 
-        $scalingHp = ($this->level-1) * $this->hpGrowth;
-        $scalingArmor = ($this->level-1) * $this->armorGrowth;
-        $scalingMr = ($this->level-1) * $this->magicResistGrowth;
-
-        $bonusHp += $scalingHp;
-        $bonusArmor += $scalingArmor;
-        $bonusMr += $scalingMr;
-
-
-        $this->maxHp = $this->baseHp + $bonusHp;
-        $this->actualHp = $this->actualHp + $bonusHp;
-        $this->armor = $this->baseArmor + $bonusArmor;
-        $this->magicResist = $this->baseMr + $bonusMr;
+    public function setLevel(int $level, $experience = null)
+    {
+        $levelDiff = $level - $this->level;
+        $this->level = $this->level + $levelDiff;
+        if (is_null($experience)) {
+            $this->experience = Champion::$experienceThreshold[$this->level - 1][0];
+        }
+        $this->updateStats();
     }
 
 }
--- tests/ChampionTest.php
+++ tests/ChampionTest.php
@@ -7,94 +7,120 @@ class ChampionTest extends TestCase
 {
     public function test_champion_can_take_physical_damage()
     {
-        $this->cwel = \App\Models\Champion::addNew('cwel', 100, 200, 200, 5, 5, 5);
-        $this->assertInstanceOf(\App\Models\Champion::class, $this->cwel);
-        $this->assertEquals(100, $this->cwel->baseHp);
-        $this->assertEquals(100, $this->cwel->actualHp);
-        $this->cwel->receivePhysicalDamage(150);
-        $this->assertEquals(100, $this->cwel->baseHp);
-        $this->assertEquals(50, $this->cwel->actualHp);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='cwel'");
-        $query->execute();
+        $champion = \App\Models\Champion::addNew('cwel', 100, 200, 200, 5, 5, 5);
+        $this->assertInstanceOf(\App\Models\Champion::class, $champion);
+        $this->assertEquals(100, $champion->baseHp);
+        $this->assertEquals(100, $champion->actualHp);
+        $champion->receivePhysicalDamage(150);
+        $this->assertEquals(100, $champion->baseHp);
+        $this->assertEquals(50, $champion->actualHp);
     }
+
     public function test_champion_can_take_magic_damage()
     {
-        $magic = \App\Models\Champion::addNew('Magic', 430,100,120,3,4,5);
-        self::assertInstanceOf(\App\Models\Champion::class, $magic);
-        self::assertEquals(430, $magic->baseHp);
-        self::assertEquals(430, $magic->actualHp);
-        $magic->receiveMagicDamage(150);
-        self::assertEquals(430, $magic->baseHp);
-        self::assertEquals(355, $magic->actualHp);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='Magic'");
-        $query->execute();
+        $champion = \App\Models\Champion::addNew('Magic', 430, 100, 120, 3, 4, 5);
+        self::assertInstanceOf(\App\Models\Champion::class, $champion);
+        self::assertEquals(430, $champion->baseHp);
+        self::assertEquals(430, $champion->actualHp);
+        $champion->receiveMagicDamage(150);
+        self::assertEquals(430, $champion->baseHp);
+        self::assertEquals(355, $champion->actualHp);
     }
+
     public function test_champion_can_add_items()
     {
-        $this->vayne = \App\Models\Champion::addNew('Vayne', 70, 30, 30, 5, 5, 5);
-        $this->assertInstanceOf(\App\Models\Champion::class, $this->vayne);
-        $this->sunfire = \App\Models\Item::find('sunfire');
-        $this->vayne->addItem($this->sunfire);
-        $this->assertCount(1, $this->vayne->items);
-        $this->assertEquals(120, $this->vayne->maxHp);
-        $this->assertEquals(40, $this->vayne->armor);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='Vayne'");
-        $query->execute();
+        $champion = \App\Models\Champion::addNew('Vayne', 70, 30, 30, 5, 5, 5);
+        $this->assertInstanceOf(\App\Models\Champion::class, $champion);
+        $item1 = \App\Models\Item::addNew('test', 50, 10, 0);
+        $champion->addItem($item1);
+        $this->assertCount(1, $champion->items);
+        $this->assertEquals(120, $champion->maxHp);
+        $this->assertEquals(40, $champion->armor);
     }
+
     public function test_champion_can_delete_items()
     {
-        $this->malfurion = \App\Models\Champion::addNew('Malfurion', 100, 10, 10, 5, 5, 5);
-        $this->assertInstanceOf(\App\Models\Champion::class, $this->malfurion);
-        $this->randuin = \App\Models\Item::find('randuin');
-        $this->malfurion->addItem($this->randuin);
-        $this->malfurion->addItem($this->randuin);
-        $this->assertCount(2, $this->malfurion->items);
-        $this->assertEquals(160, $this->malfurion->maxHp);
-        $this->assertEquals(50, $this->malfurion->armor);
-        $this->malfurion->deleteItem('randuin');
-        $this->assertCount(1, $this->malfurion->items);
-        $this->assertEquals(130, $this->malfurion->maxHp);
-        $this->assertEquals(30, $this->malfurion->armor);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='Malfurion'");
-        $query->execute();
+        $champion = \App\Models\Champion::addNew('Malfurion', 100, 10, 10, 5, 5, 5);
+        $this->assertInstanceOf(\App\Models\Champion::class, $champion);
+        $item2 = \App\Models\Item::addNew('test', 30, 20, 5);
+        $champion->addItem($item2);
+        $champion->addItem($item2);
+        $this->assertCount(2, $champion->items);
+        $this->assertEquals(160, $champion->maxHp);
+        $this->assertEquals(50, $champion->armor);
+        $champion->deleteItem($item2->name);
+        $this->assertCount(1, $champion->items);
+        $this->assertEquals(130, $champion->maxHp);
+        $this->assertEquals(30, $champion->armor);
     }
 
     public function test_champion_can_add_level()
     {
-        $granny = \App\Models\Champion::addNew('Granny',120,2,21,3,4,5);
-        self::assertInstanceOf(\App\Models\Champion::class, $granny);
-        $granny->addLevel();
-        self::assertEquals(2,$granny->level);
-        self::assertEquals(123, $granny->maxHp);
-        self::assertEquals(25, $granny->armor);
-        self::assertEquals(7, $granny->magicResist);
+        $champion = \App\Models\Champion::addNew('Granny', 120, 2, 21, 3, 4, 5);
+        self::assertInstanceOf(\App\Models\Champion::class, $champion);
+        $champion->addLevel();
+        self::assertEquals(2, $champion->level);
+        self::assertEquals(123, $champion->maxHp);
+        self::assertEquals(25, $champion->armor);
+        self::assertEquals(7, $champion->magicResist);
     }
 
     public function test_champion_can_set_levels()
     {
-        $granny = \App\Models\Champion::addNew('Granny',120,2,21,3,4,5);
-        self::assertInstanceOf(\App\Models\Champion::class, $granny);
-        $granny->setLevel(16);
-        self::assertEquals(165, $granny->maxHp);
-        self::assertEquals(81, $granny->armor);
-        self::assertEquals(77, $granny->magicResist);
-        self::assertEquals(16, $granny->level);
-        self::assertEquals(1500, $granny->experience);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='Granny'");
-        $query->execute();
+        $champion = \App\Models\Champion::addNew('Granny', 120, 2, 21, 3, 4, 5);
+        self::assertInstanceOf(\App\Models\Champion::class, $champion);
+        $champion->setLevel(16);
+        self::assertEquals(165, $champion->maxHp);
+        self::assertEquals(81, $champion->armor);
+        self::assertEquals(77, $champion->magicResist);
+        self::assertEquals(16, $champion->level);
+        self::assertEquals(1500, $champion->experience);
     }
 
     public function test_champion_can_add_experience()
     {
-        $granny = \App\Models\Champion::addNew('Granny',120,2,21,3,4,5);
-        self::assertInstanceOf(\App\Models\Champion::class, $granny);
-        $granny->addExperience(560);
-        self::assertEquals(6, $granny->level);
-        self::assertEquals(560, $granny->experience);
-        self::assertEquals(135, $granny->maxHp);
-        self::assertEquals(41, $granny->armor);
-        self::assertEquals(27, $granny->magicResist);
-        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where name='Granny'");
+        $champion = \App\Models\Champion::addNew('Granny', 120, 2, 21, 3, 4, 5);
+        self::assertInstanceOf(\App\Models\Champion::class, $champion);
+        $champion->addExperience(560);
+        self::assertEquals(6, $champion->level);
+        self::assertEquals(560, $champion->experience);
+        self::assertEquals(135, $champion->maxHp);
+        self::assertEquals(41, $champion->armor);
+        self::assertEquals(27, $champion->magicResist);
+    }
+
+    public function test_champion_with_items_and_level()
+    {
+        $championName = 'Granny';
+        $baseHp = 120;
+        $champion = \App\Models\Champion::addNew($championName, $baseHp, 2, 25, 3, 4, 5);
+        self::assertInstanceOf(\App\Models\Champion::class, $champion);
+        self::assertEquals($baseHp, $champion->actualHp);
+        self::assertEquals($baseHp, $champion->maxHp);
+        $champion->receivePhysicalDamage(120);
+        $hpDiff = $baseHp - $champion->actualHp;
+        $expectedDiff = 96;
+        self::assertEquals($expectedDiff, $hpDiff);
+        self::assertEquals($baseHp - $hpDiff, $champion->actualHp);
+        self::assertEquals($baseHp, $champion->maxHp);
+
+        $item3 = \App\Models\Item::addNew('test', 100, 45, 15);
+        $item2 = \App\Models\Item::addNew('test2', 30, 20, 5);
+        $item1 = \App\Models\Item::addNew('test3', 50, 10, 0);
+        $champion->addItem($item3);
+        $champion->addItem($item2);
+        $champion->addItem($item1);
+
+        self::assertEquals($baseHp + 180, $champion->maxHp);
+        self::assertEquals($champion->maxHp - $hpDiff, $champion->actualHp);
+    }
+
+    public function tearDown(): void
+    {
+        $query = \App\Services\Db::get()->prepare("Delete from lelek.champions where 1=1");
         $query->execute();
+        $query = \App\Services\Db::get()->prepare("Delete from lelek.items where 1=1");
+        $query->execute();
+        parent::tearDown(); // TODO: Change the autogenerated stub
     }
 }
